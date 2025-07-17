3fb8cf4cca722d9b704bfc469d3880b921979a91
Ayman Abdelgadir <xxdayman@gmail.com>
2025-07-18 00:17:40 +0300

    implement Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import HomePage from "./pages/HomePage.jsx";
-import LoadingSpinner from "./components/LoadingSpinner.jsx";
+
 
 const App = () => {
     return (
        <>
           <HomePage/>
-           <LoadingSpinner/>
+
        </>
     );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,26 +1,29 @@
 import React, {useEffect, useState} from 'react';
 import {options} from '../utils/api.js';
 import axios from 'axios';
+import LoadingSpinner from '../components/LoadingSpinner.jsx';
 
 const HomePage = () => {
    const addData =  (res) => {
        setMovies(res.data.results)
    }
 
-
+    const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState([]);
 
 
     useEffect(() => {
         axios(options)
             .then(res => addData(res))
+            .then(res => console.log(res))
             .catch(err => console.log(err))
-            .finally(() => console.log('done'))
+            .finally(() => setLoading(false))
     },[])
     return (
 
         <section>
-
+            {loading ? <LoadingSpinner/> :<div>Done</div>
+            }
         </section>
     );
 };
